799d79803ca788ad6e9134e151615d0b2db832f8
phoenix <phoenix0x.web3@gmail.com>
2025-11-07 02:09:58 +0400

    Minor: fix "Could not fetch SOL price for USD balance calculation from Binance. Proxy issue"

--- libs/base_sol.py
+++ libs/base_sol.py
@@ -86,6 +86,9 @@ class Base:
         for token, balance in balances.items():
             if token == TokenContracts.SOL:
                 sol_price = await self.get_token_price(token_symbol="SOL")
+                if sol_price is None:
+                    raise ValueError("Could not fetch SOL price for USD balance calculation from Binance. Proxy issue")
+                
                 usd_balanced[token] = float(balance.Ether) * sol_price
             else:
                 usd_balanced[token] = float(balance.Ether)
